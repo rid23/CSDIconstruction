7b863f24f2133d7590e23a4b5dc5e27b7d5fedd7
rohan37-pro <rohan2002maji@gmail.com>
2023-04-28 20:24:24 +0530

    why choose our course

--- csdi-frontend/src/App.js
+++ csdi-frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Navbarsecond/>
       <Banner/>
       <Programs/>
+      <Whychooseus/>
     </div>
   );
 }
@@ -39,16 +40,25 @@ function Programs() {
   )
 }
 
-function whychooseus() {
+function Whychooseus() {
 
   return (
     <>
       <div className='features-container'>
         <div className='feature-paper'>
-          
+          <p>We are Internationally Recognised Centre of EAL UK, first of its kind in India (Code: C001338). EAL is the specialist skills partner and awarding organisation for industry. It supports us in providing high quality, fit for purpose certification which meet learners’ needs & ensure satisfaction. Our training, examination, admission & placement processes are internationally audited. This makes us one of the premier construction management colleges in India.</p>
         </div>
-        <div className='feature'>
-
+        <div className='feature-paper'>
+            <p>Importantly, each of the students earn job of his/her choice. Another reason why we are one of the top construction management colleges in Pune is because we provide a platform to join Planning / Execution/ QA-QC / Estimation & Billing posts with 18000 to 35000/- pm or even more salary that commensurate with your caliber, expertise and experience. Students of earlier batches are 100% placed. This can be considered as a ladder to reach Project Manager’s position in 7 to 8 yrs. with relevant experience in the field of Construction Project Management.</p>
+        </div>
+        <div className='feature-paper'>
+          <p>Our courses are designed especially for civil engineers to bridge the gap between theory and practice. Predefined 151 employability skills from Construction Project Management are imbibed into each civil engineer in a systematic way in 1 yr. PGP through site visits, classroom training, out bound trainings, events, case studies, and practical. So, we’ve got another reason to proudly say that we are one of the construction management colleges in India, as, although there are many institutes offering construction management courses in India, none of the construction management institutes in India focuses on these many skills.</p>
+        </div>
+        <div className='feature-paper'>
+          <p>Curriculum and pedagogy of our programs are precisely set to have focus on the development of right set of employability skills needed for civil engineers. Latest knowledge and practices followed in Construction Project Management are included in the curriculum. Our pedagogy helps transform a learner into a skilled & employable civil engineer, and therefore places us into the league of top 10 construction management institutes in India.</p>
+        </div>
+        <div className='feature-paper'>
+          <p>With the holistic view in mind, we provide learners with ideal study environment required to evolve all round personality that includes site visits, yoga, sports, mediation, out bound trainings, personality grooming & counselling in addition to high end technical knowledge sharing. Assessments are skills based; turning their approach from theory to practice. And hence we can claim that we are one of the top 10 construction management education institutes in India.</p>
         </div>
       </div>
     </>
