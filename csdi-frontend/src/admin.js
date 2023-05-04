b10a4da3aaefd8134700ae142c52c6bdb211dd7c
pritam62-pro <pritambiswas62960737@gmail.com>
2023-05-04 16:15:12 +0530

    faculty page responsive

--- csdi-frontend/src/admin.js
+++ csdi-frontend/src/admin.js
@@ -1,11 +1,18 @@
 import React from "react";
 import AdminSideNav from "./adminsidenav";
+import "./admin.css"
 
 function Adminhome() {
 
     return (
         <>
-        <AdminSideNav/>
+        <div className="Admin-home">
+            <AdminSideNav/>
+            <div className="admin-playground">
+                <h1> Administrator Page</h1>
+            </div>
+        </div>
+        
         </>
     )
 }
--- csdi-frontend/src/adminlogin.js
+++ csdi-frontend/src/adminlogin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./adminlogin.css"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 function Adminlogin() {
@@ -7,6 +8,9 @@ function Adminlogin() {
     return (
         <div className="admin-login-page">
             <div className="admin-login-form">
+                <div className="admin-login-person-icon">
+                    <AccountCircleIcon/>
+                </div>
                 <label className="admin-login-lebel">User Name</label>
                 <input className="admin-login-input" placeholder="Enter Username"/>
                 <label className="admin-login-lebel" >Password</label>
